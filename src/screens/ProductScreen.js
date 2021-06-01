15def98553424b0fb7ff6c77d55af4a574239132
Gama <gustavo.p.gama@gmail.com>
2021-06-01 02:18:57 +0200

    changes btns4

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -4,6 +4,28 @@ import Rating from '../components/Rating';
 import data from '../data';
 
 
+var callAPI = (firstName,lastName)=>{
+  // instantiate a headers object
+  var myHeaders = new Headers();
+  // add content type header to object
+  myHeaders.append("Content-Type", "application/json");
+  // using built in JSON utility package turn object to string and store in a variable
+  var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
+  // create a JSON object with parameters for API call and store in a variable
+  var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+  };
+  // make API call with parameters and use promises to get response
+  fetch("https://n3gbqg4hh3.execute-api.us-east-1.amazonaws.com/dev/", requestOptions)
+  .then(response => response.text())
+  .then(result => alert(JSON.parse(result).body))
+  .catch(error => console.log('error', error));
+}
+
+
 
 export default function ProductScreen(props) {
   const product = data.products.find((x) => x._id === props.match.params.id);
